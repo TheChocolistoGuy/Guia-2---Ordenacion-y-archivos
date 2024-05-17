60556b83226853da96e13a6ef40b20d9adb32dda
TheChocolistoGuy <felipemoskg@gmail.com>
2024-05-17 15:12:57 -0500

    actualizacion 1
    Implementacion de archivos para las definiciones, pistas y respuestas correctas

--- GUIA 1 Juego de Deletreo.cpp
+++ GUIA 1 Juego de Deletreo.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <stdlib.h>     
-#include <time.h>       
+#include <time.h>
+#include <fstream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -10,55 +11,101 @@ int num_jugadores;
 vector <string> jugadores;
 string otra_vez;
 bool salir;
+bool correcto;
+ifstream arch;
 
-string directorio[30][3] = {
-							{"scissors","A device used for cutting materials such as paper, cloth, and hair, consisting of two sharp blades that are joined in the middle, and two handles with holes to put your fingers through","tijeras"},
-							{"glue", "A sticky substance that is used for joining things together permanently, produced from animal bones and skins or by a chemical process", "pegamento"},
-							{"notebook", "A book of plain paper or paper with lines, for writing on", "cuaderno"},
-							{"folder", "a piece of plastic or cardboard folded down the middle and used for keeping loose papers in", "carpeta"}
-							,{"highlighter", "A special pen containing bright ink, used to mark words in a book, magazine, etc.", "resaltador"}
-							,{"crayons", "A small stick of coloured wax used for drawing or writing", "crayones"}
-							,{"compass", "A V-shaped device that is used for drawing circles or measuring distances on maps", "compás"}
-							,{"chalk", "A type of soft rock or a similar substance used for writing or drawing", "tiza"}
-							,{"schedule", "A list of planned activities or things to be done showing the times or dates when they are intended to happen or be done.", "horario"}
-							,{"dictionary", "A book that contains a list of words in alphabetical order and explains their meanings, or gives a word for them in another language", "diccionario"}
-							,{"syringe", "Small tube with a fine hollow needle, used for injecting drugs or for taking blood from someone's body.", "jeringa"}
-							,{"glasses", "Lenses mounted in a frame that holds them in front of a person's eyes", "gafas"}
-							,{"bottle", "Is a glass or plastic container in which drinks and other liquids are kept", "botella"}
-							,{"heart", "Is the main organ of your cardiovascular system", "corazón"}
-							,{"bullet", "A small metal projectile", "bala"}
-							,{"sword", "A hand weapon having a long, sharp-pointed Blade", "espada"}
-							,{"dishes", "All the objects that have been used to cook, serve, and eat a meal", "platos"}
-							,{"kite", "It consists of a light frame covered with paper or cloth and has a long string", "cometa"}
-							,{"lock", "Is a tool that keeps a room, house, drawer, or box securely closed.", "candado"}
-							,{"door", "It serves to prevent entry and exit, to close or open a closet or piece of furniture.", "puerta"}
-							,{"floor", "Natural or artificial pavement of rooms, streets, paths, etc.", "suelo"}
-							,{"garden", "Land where plants are grown.", "jardin"}
-							,{"kitchen", "Room or place in the house in which food is cooked.", "cocina"}
-							,{"wall", "Plate of any material with which a space is divided or closed", "muro"}
-							,{"window", "Opening in a wall or wall where an element is placed and which generally serves to look out and provide light and ventilation.", "ventana"}
-							,{"oven", "Closed metal device, inside which food is roasted, cooked, heated or gratinated", "horno"}
-							,{"spoon", "Utensil that consists of an extended concave part in a handle, and that is used, especially, to put liquid, soft or small things in the mouth.", "cuchara"}
-							,{"table", "Furniture essentially formed by a smooth surface supported by one or more feet and that serves different uses", "mesa"}
-							,{"lighter", "A small device that produces a flame that is used to light cigarettes", "encendedor"}
-							,{"letter", "a written message from one person to another, usually put in an envelope and sent by post", "carta"}
-							};
-							
 int puntuaciones[4][2] = {
 						{0 ,0},
 						{0 ,0},
 						{0 ,0},
 						{0 ,0}
 						};
-						
-		
-		
+					
+ifstream abrir_archivo (const string& nombre)
+{
+	ifstream arch;
+	arch.open(nombre, ios::in);
+	if (!arch.is_open())
+	{
+    	cerr << "No se pudo abrir el archivo." << endl;
+		exit(0);
+    }
+	return arch;
+}
+
+void leer_definicion(int numero_pregunta)
+{
+	string definicion;
+	string nombrearchivo = "definiciones.txt";
+
+	ifstream archivo = abrir_archivo(nombrearchivo);
+
+    int numeroActualLinea = 0;
+
+    while (getline(archivo, definicion)) 
+	{
+        numeroActualLinea++;
+        if (numeroActualLinea == numero_pregunta) 
+		{
+            cout << definicion << endl;
+            break;
+        }
+    }
+
+    archivo.close();
+}
+
+string leer_pista(int numero_pregunta)
+{
+	string pista;
+	string linea;
+	string nombrearchivo = "pistas.txt";
+
+	ifstream archivo = abrir_archivo(nombrearchivo);
+    int numeroActualLinea = 0;
+
+    while (getline(archivo, linea)) 
+	{
+        numeroActualLinea++;
+        if (numeroActualLinea == numero_pregunta) 
+		{
+            pista=linea;
+            break;
+        }
+    }
+
+    archivo.close();
+	return pista;
+}
+
+void leer_respuesta(int numero_pregunta, string Respuesta)
+{
+	string respuesta_correcta;
+	string nombrearchivo = "palabras_correctas.txt";
+
+	ifstream archivo = abrir_archivo(nombrearchivo);
+
+    int numeroActualLinea = 0;
+
+    while (getline(archivo, respuesta_correcta)) {
+        numeroActualLinea++;
+        if (numeroActualLinea == numero_pregunta) {
+            if (respuesta_correcta == Respuesta)
+			{
+				correcto=true;
+			}
+            break;
+        }
+    }
+
+    archivo.close();
+}
+
 int modo1 (int x)
 {	
 	string palabra_usuario;
 	string palabra_correcta;
 	bool pista=false;
-	int numero_aleatorio;
 	int renglon;
 	int ganador;
 	int rango=30;
@@ -71,11 +118,12 @@ int modo1 (int x)
 		for (int turno=0 ; turno<x ; turno++)
 		{
 			pista=false;
+			correcto=false;
 			aleatorio=rand() % rango;
 			cout<<"Ahora es el turno de "<<jugadores[turno]<<". A que palabra en ingles corresponde la siguiente definicion?:"<<endl;
 			cout<<endl;
-			cout<<directorio[aleatorio][1];
-			palabra_correcta=directorio[aleatorio][0];
+			leer_definicion(aleatorio);
+			// palabra_correcta=directorio[aleatorio][0];
 			cout<<endl;
 			cout<<"ingresa tu respuesta (si necesitas una pista ingresa la palabra 'pista'):"<<endl;
 			cin>>palabra_usuario;
@@ -84,16 +132,22 @@ int modo1 (int x)
 			if (palabra_usuario== "pista")
 			{
 				cout<<endl;
-				cout<<"La palabra que tienes que adivinar, al traducirla al espanol es: "<<directorio[aleatorio][2]<<endl;
+				cout<<"La palabra que tienes que adivinar, al traducirla al espanol es: "<< leer_pista(aleatorio) <<endl;
 				pista=true;
 				cout<<endl;
 				
 				cout<<"ahora si, ingresa tu respuesta final: "<<endl;
 				cin>>palabra_usuario;
 				cout<<endl;
+				leer_respuesta(aleatorio, palabra_usuario);
 			}
-			
-			if(palabra_usuario==palabra_correcta and pista==false)
+
+			else
+			{
+				leer_respuesta(aleatorio, palabra_usuario);
+			}
+
+			if( correcto==true and pista==false)
 			{
 				cout<<"Respuesta correcta !felicitaciones!, has ganado 1000 puntos."<<endl;
 				cout<<endl;
@@ -104,7 +158,7 @@ int modo1 (int x)
 				
 			else
 			{
-				if(palabra_usuario==palabra_correcta and pista==true)
+				if( correcto==true and pista==true)
 					{
 						cout<<"Respuesta correcta !felicitaciones!, has ganado 500 puntos por haber usado una pista."<<endl;
 						cout<<endl;
@@ -115,7 +169,7 @@ int modo1 (int x)
 				
 				else
 				{
-					if (palabra_usuario!=palabra_correcta and pista==false)
+					if ( correcto==false and pista==false)
 					{
 						cout<<"Respuesta incorrecta, pierdes 500 puntos."<<endl;
 						cout<<endl;
@@ -126,7 +180,7 @@ int modo1 (int x)
 
 					else
 					{
-						if(palabra_usuario!=palabra_correcta and pista==true)
+						if( correcto==false and pista==true)
 						{
 						cout<<"respuesta incorrecta, pierdes 1000 puntos por haber usado una pista."<<endl;
 						cout<<endl;
